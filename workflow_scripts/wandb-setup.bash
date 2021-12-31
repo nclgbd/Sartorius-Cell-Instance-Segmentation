e49432de16bac408a80002dd3991dc6f5a4e265b
Nicole Gu <nicoleguob@gmail.com>
2021-12-31 12:34:09 -0800

    Changed tag naming convention

--- workflow_scripts/wandb-setup.bash
+++ workflow_scripts/wandb-setup.bash
@@ -1,7 +1,6 @@
 echo "wandb_resume=never
-wandb_run_id=$1
 wandb_mode=online
-wandb_job_type=kfold
+wandb_job_type=$1
 wandb_tags=baseline
 wandb_entity=nclgbd
 wandb_project=Sartorius-Kaggle-Competition

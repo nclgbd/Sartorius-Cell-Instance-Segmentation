c774e587cb49d757a1cd968494c8c80a4149a090
Nicole Gu <nicoleguob@gmail.com>
2021-12-30 21:07:49 -0800

    Modified wandb

--- workflow_scripts/wandb-setup.bash
+++ workflow_scripts/wandb-setup.bash
@@ -1,8 +1,8 @@
-echo "wandb_resume=allow
+echo "wandb_resume=never
 wandb_run_id=$1
 wandb_mode=online
 wandb_job_type=kfold
 wandb_tags=baseline
 wandb_entity=nclgbd
 wandb_project=Sartorius-Kaggle-Competition
-wandb_api_key=$2" >> "$3"
+wandb_api_key=$2" >>"$3"

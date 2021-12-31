1a96d6946c5395193ff904a370b432549879dc28
Nicole Gu <nicoleguob@gmail.com>
2021-12-30 18:16:21 -0800

    Finished making reusable scripts

---
+++ workflow_scripts/wandb-setup.bash
@@ -0,0 +1,7 @@
+echo "wandb_resume=allow >> config/.env
+wandb_run_id=36u8cxrw >> config/.env
+wandb_mode=online >> config/.env
+wandb_job_type=kfold >> config/.env
+wandb_tags=baseline >> config/.env
+wandb_entity=nclgbd >> config/.env
+wandb_project=Sartorius-Kaggle-Competition" >>config/.env
